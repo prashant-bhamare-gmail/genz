104530bf7773cbbc1c544c56ab8e4928f3cb2051
Pramod <pramodsharma2696@gmail.com>
2024-11-29 13:12:26 +0530

    commit landing pages chnages

--- resources/views/body/footer.blade.php
+++ resources/views/body/footer.blade.php
@@ -1,18 +1,9 @@
-<footer id="footer" class="bg-color-dark-100 border border-end-0 border-start-0 border-bottom-0 border-color-light-3 mt-0">
-				<div class="container text-center my-3 py-5">
-					<a href="index.html">
-						<img src="{{ asset('') }}img/lazy.png" data-src="{{ asset('') }}img/landing/logo.png" width="102" height="45" class="appear-animation lazyload mb-4" alt="Porto" data-appear-animation="fadeIn" data-appear-animation-delay="300">
-					</a>
-					<p class="text-4 mb-4">Porto is exclusively available on themeforest.net by <a href="https://themeforest.net/user/okler/" class="text-color-light text-decoration-none" target="_blank">Okler.</a></p>
-					<ul class="social-icons social-icons-big social-icons-dark-2">
-						<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="social-icons-x"><a href="http://www.x.com/" target="_blank" title="X"><i class="fab fa-x-twitter"></i></a></li>
-						<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-					</ul>
-				</div>
-				<div class="copyright bg-color-dark-100 py-4">
-					<div class="container text-center py-2">
-						<p class="mb-0 text-2">Copyright 2013 - 2024 - Porto - All Rights Reserved</p>
+<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-center pb-4">
+							<p>2024 © Porto <span class="text-color-light">Business Consulting</span> - Copyright All Rights Reserved</p>
+						</div>
 					</div>
 				</div>
 			</footer>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,52 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
+<head>
 
-		<!-- Basic -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Basic -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Porto - Multipurpose Website Template</title>	
+<title>Demo Business Consulting | Porto - Multipurpose Website Template</title>	
 
-		<meta name="keywords" content="WebSite Template" />
-		<meta name="description" content="Porto - Multipurpose Website Template">
-		<meta name="author" content="okler.net">
+<meta name="keywords" content="WebSite Template" />
+<meta name="description" content="Porto - Multipurpose Website Template">
+<meta name="author" content="okler.net">
 
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{ asset('') }}img/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="{{ asset('') }}img/apple-touch-icon.png">
 
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 
-		<!-- Web Fonts  -->
-		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7COpen+Sans:400,700,800&display=swap" rel="stylesheet" type="text/css">
+<!-- Web Fonts  -->
+<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700%7CSintony:400,700&display=swap" rel="stylesheet" type="text/css">
 
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="{{ asset('') }}vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/fontawesome-free/css/all.min.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/animate/animate.compat.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/simple-line-icons/css/simple-line-icons.min.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/owl.carousel/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/owl.carousel/assets/owl.theme.default.min.css">
-		<link rel="stylesheet" href="{{ asset('') }}vendor/magnific-popup/magnific-popup.min.css">
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="{{ asset('') }}vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/animate/animate.compat.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/simple-line-icons/css/simple-line-icons.min.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/owl.carousel/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/owl.carousel/assets/owl.theme.default.min.css">
+<link rel="stylesheet" href="{{ asset('') }}vendor/magnific-popup/magnific-popup.min.css">
 
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="{{ asset('') }}css/theme.css">
-		<link rel="stylesheet" href="{{ asset('') }}css/theme-elements.css">
-		<link rel="stylesheet" href="{{ asset('') }}css/theme-blog.css">
-		<link rel="stylesheet" href="{{ asset('') }}css/theme-shop.css">
+<!-- Theme CSS -->
+<link rel="stylesheet" href="css/theme.css">
+<link rel="stylesheet" href="css/theme-elements.css">
+<link rel="stylesheet" href="css/theme-blog.css">
+<link rel="stylesheet" href="css/theme-shop.css">
 
-		<!-- Demo CSS -->
-		<link rel="stylesheet" href="{{ asset('') }}css/demos/demo-landing.css">
+<!-- Demo CSS -->
+<link rel="stylesheet" href="css/demos/demo-business-consulting.css">
 
-		<!-- Skin CSS -->
-		<link id="skinCSS" rel="stylesheet" href="{{ asset('') }}css/skins/skin-landing.css">
+<!-- Skin CSS -->
+<link id="skinCSS" rel="stylesheet" href="css/skins/skin-business-consulting.css">
 
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="{{ asset('') }}css/custom.css">
+<!-- Theme Custom CSS -->
+<link rel="stylesheet" href="css/custom.css">
 
-	</head>
+</head>
 	<body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 0, 'effect': 'pulse'}">
 		<div class="loading-overlay">
 			<div class="bounce-loader">
